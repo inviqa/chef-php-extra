030a67c3558c5d9a5c39596943536d4472832cdd
Alistair Stead <alistair.stead@designdisclosure.com>
2012-07-08 18:50:11 +0100

    Initial commit of the php-extra chef cookbook

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Alistair Stead"
+maintainer_email "alistair.stead@designdisclosure.com"
+license          "Apache 2.0"
+description      "Installs/Configures chef-php-extra"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
