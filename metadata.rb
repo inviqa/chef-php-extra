0879effa4d58c0eb9239c52d74646b8979c036cf
Jakub Zalas <jakub@zalas.pl>
2013-02-08 11:49:03 +0000

    Bumped opscode php cookbook version to 1.1.8.

--- metadata.rb
+++ metadata.rb
@@ -8,7 +8,7 @@ version          "0.0.2"
 depends "git", "= 1.0.0"
 depends "yum", "= 0.8.0"
 depends "apt", "= 1.4.6"
-depends "php", "= 1.0.2"
+depends "php", "= 1.1.8"
 depends "chef-php-extra"
 
 %w{ ubuntu, debian, centos, redhat, fedora }.each do |os|
